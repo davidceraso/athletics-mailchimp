f12b8afba742a4df44aa4f5f28b7421cc306fb7b
David Ceraso <davidceraso@athleticsnyc.com>
2014-10-09 09:14:29 -0400

    Starting new custom html for Athletics.

---
+++ webroot/templates/template-2/data.php
@@ -0,0 +1,50 @@
+<?php
+
+
+$stories = array(
+	array(
+		'img' =>  'assets/img/mls.jpg',
+		'title' => 'Rebranding Major League Soccer',
+		'copy' => array(
+			'We are excited to announce our rebrand of Major League Soccer. The identity needed to reflect the growing love for soccer in North America, and be a mark that both teams and fans could adopt, and make their own.', 
+			'We are excited to announce our rebrand of Major League Soccer. The identity needed to reflect the growing love for soccer in North America, and be a mark that both teams and fans could adopt, and make their own.'
+			),
+		'external' => array(
+			'www.berlinerbenson.com', 'www.gigundagroup.com'
+		) 
+ 	),
+ 	array(
+ 		'img' =>  null,
+		'title' => 'Say Hello to StartUp',
+		'copy' => array(
+			'A new podcast series from the minds of @abexlumberg & @mlieber. Keep up @podcaststartup.'
+		),
+		'external' => array(
+			'www.hearstartup.com'
+		) 
+ 	),
+ 	array(
+ 		'img' =>  null,
+		'title' => 'Wordpress Big Media',
+		'copy' => array(
+			'The team at @athleticsnyc presents on building a contributor network on WordPress.'
+		),
+		'external' => array(
+		)
+ 	),
+ 	array(
+ 		'img' =>  null,
+		'title' => 'Rebranding Major League Soccer',
+		'copy' => array(
+			'We are excited to announce our rebrand of Major League Soccer. The identity needed to reflect the growing love for soccer in North America, and be a mark that both teams and fans could adopt, and make their own.'
+		),
+		'external' => array(
+		)
+ 	)
+);
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+return array(
+	'stories' => $stories
+);
