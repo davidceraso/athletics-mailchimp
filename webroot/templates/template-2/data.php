546be1f08b1db71f728107d056d59e05d2e7aa30
David Ceraso <davidceraso@athleticsnyc.com>
2014-10-09 11:22:53 -0400

    Removing external links (i.e. hearstartup.com). Adding html tags into the data content section.

--- webroot/templates/template-2/data.php
+++ webroot/templates/template-2/data.php
@@ -2,43 +2,54 @@
 
 
 $stories = array(
-	array(
-		'img' =>  'assets/img/mls.jpg',
-		'title' => 'Rebranding Major League Soccer',
-		'copy' => array(
-			'We are excited to announce our rebrand of Major League Soccer. The identity needed to reflect the growing love for soccer in North America, and be a mark that both teams and fans could adopt, and make their own.', 
-			'We are excited to announce our rebrand of Major League Soccer. The identity needed to reflect the growing love for soccer in North America, and be a mark that both teams and fans could adopt, and make their own.'
-			),
-		'external' => array(
-			'www.berlinerbenson.com', 'www.gigundagroup.com'
-		) 
- 	),
+
  	array(
  		'img' =>  null,
 		'title' => 'Say Hello to StartUp',
-		'copy' => array(
-			'A new podcast series from the minds of @abexlumberg & @mlieber. Keep up @podcaststartup.'
-		),
-		'external' => array(
-			'www.hearstartup.com'
-		) 
- 	),
- 	array(
- 		'img' =>  null,
-		'title' => 'Wordpress Big Media',
-		'copy' => array(
-			'The team at @athleticsnyc presents on building a contributor network on WordPress.'
-		),
-		'external' => array(
+		'content' => array(
+			'<p>
+				A new podcast series from the minds of  <a href="#">@abexlumberg</a> &  <a href="#">@mlieber</a>. Keep up  <a href="#">@podcaststartup</a>.
+			</p>'
 		)
  	),
  	array(
  		'img' =>  null,
+		'title' => 'Wordpress Big Media',
+		'content' => array(
+			'<p>The team at  <a href="#">@athleticsnyc</a> presents on building a contributor network on WordPress.'
+		)
+ 	),
+	array(
+		'img' =>  'assets/img/mls.jpg',
 		'title' => 'Rebranding Major League Soccer',
-		'copy' => array(
-			'We are excited to announce our rebrand of Major League Soccer. The identity needed to reflect the growing love for soccer in North America, and be a mark that both teams and fans could adopt, and make their own.'
-		),
-		'external' => array(
+		'content' => array(
+			'<p>
+				We are excited to announce our <a href="#">rebrand</a> of Major League Soccer. The  <a href="#">identity</a> needed to reflect the growing love for soccer in North America, and be a mark that both teams and fans could adopt, and make their own.
+			</p>', 
+			'<p>
+				We are excited to announce our rebrand of <a href="#">Major League Soccer</a>. The identity needed to reflect the growing love for soccer in North America, and be a mark that both teams and fans could adopt, and make their own. The identity needed to reflect the growing love for soccer in North America, and be a mark that both teams and fans could adopt, and make their own.
+			</p>',
+			'<ul>
+				<li>
+					We are excited to announce our <a href="#">rebrand</a> of Major League Soccer. The  <a href="#">identity</a> needed to reflect the growing love for soccer in North America, and be a mark that both teams and fans could adopt, and make their own.
+				</li>
+				<li>
+					We are excited to announce our rebrand of <a href="#">Major League Soccer</a>. The identity needed to reflect the growing love for soccer in North America, and be a mark that both teams and fans could adopt, and make their own. The identity needed to reflect the growing love for soccer in North America, and be a mark that both teams and fans could adopt, and make their own.
+				</li>
+				<li>
+					We are excited to announce our <a href="#">rebrand</a> of Major League Soccer.
+				</li>
+			</ul>'
+			)
+
+	),
+ 	array(
+ 		'img' =>  null,
+		'title' => 'Rebranding Major League Soccer',
+		'content' => array(
+			'<p>
+				We are excited to announce our rebrand of Major League Soccer. The identity needed to reflect the growing love for soccer in North America, and be a mark that both teams and fans could adopt, and make their own.
+			</p>'
 		)
  	)
 );
