6340181876a9deea14c7247f8c560413e8d38e20
David Ceraso <davidceraso@athleticsnyc.com>
2014-10-10 16:11:46 -0400

    Adding a new template for sign up forms.

---
+++ webroot/templates/signup-1/data.php
@@ -0,0 +1,87 @@
+<?php
+
+
+$stories = array(
+
+ 	array(
+ 		'slug' => 'MAJOR CHANGES',
+ 		'img' =>  'assets/img/mls.jpg',
+		'title' => 'Club, Country, Community: Rebranding Major League Soccer',
+		'content' => array(
+			'<p>
+				We’re delighted for the launch of the new brand identity system for Major League Soccer. You can explore details of the system at <a href="http://www.mlssoccer.com/next" target="_blank">MLS Next</a> and on  <a href="http://athleticsnyc.com/mls/" target="_blank">our site</a>. The folks at Brand New published a very <a href="http://www.underconsideration.com/brandnew/archives/new_logo_for_mls_by_athletics_and_berliner_benson.php" target="_blank">thoughtful review</a>.
+			</p>'
+		)
+ 	),
+ 	array(
+ 		'slug' =>  'It\'s a Start',
+ 		'img' =>  'assets/img/startup.jpg',
+		'title' => 'What happens when someone who knows nothing about business starts one?',
+		'content' => array(
+			'<p>
+				Introducing <a href="http://hearstartup.com/" target="_blank">StartUp</a>, a new podcast series from Alex Blumberg. It’s the real-time account of launching a new business. Athletics is happy to be along for the ride. Be sure to <a href="https://itunes.apple.com/us/podcast/startup-podcast/id913805339?mt=2&ls=1" target="_blank">subscribe on iTunes</a>.
+			</p>
+			<p>
+				And on the topic of podcasts...
+			</p>',
+			'<ul>
+				<li>
+					The fine folks at <a href="http://www.marketplace.org/shows/marketplace-weekend" target="_blank">Marketplace Weekend</a> included a segment on Athletics partner <a href="https://twitter.com/jamesellisnyc" target="_blank">James Ellis</a>’ 19-yrs-old, just-moved-to-NYC story. Cleverly titled, <a href="http://www.marketplace.org/topics/your-money/appetite-vocation" target="_blank">Appetite for Vocation</a>.
+				</li>
+				<li>
+					Don’t miss <a href="http://songexploder.net/" target="_blank">Song Exploder</a>, a (relatively) new podcast from <a href="https://twitter.com/HrishiHirway" target="_blank">Hrishikesh Hirway</a>. <a href="http://songexploder.net/episode-7-jeff-beal/" target="_blank">Episode 7</a> is a great place to start.
+				</li>
+				<li>
+					Marco Arment’s <a href="https://overcast.fm/" target="_blank">Overcast</a> is our new favorite podcasting app.
+				</li>
+			</ul>'
+		)
+ 	),
+	array(
+		'slug' =>  null,
+		'img' =>  'assets/img/wp.jpg',
+		'title' => 'Wordpress shop talk',
+		'content' => array(
+			'<p>
+				First, we had the pleasure of speaking at September’s <a href="http://www.meetup.com/Big-Media-WordPress-Meetup/events/185953602/" target="_blank">Big Media Meetup</a> about our experience building <a href="http://thoughtcatalog.com/" target="_blank">Thought Catalog</a> on the Wordpress <a href="http://vip.wordpress.com/" target="_blank">VIP platform</a>. In particular, we got into the weeds about URLs and how we provide Thought Catalog’s 7k+ writers with lovely URLs <a href="http://thoughtcatalog.com/daniel-coffeen/" target="_blank">like this</a>. Those familiar with Wordpress’ router will appreciate <a href="https://github.com/athletics/wp-authors-first" target="_blank">WP Authors First</a>, an open-source plugin that unpacks the magic necessary to enable Thought Catalog’s permalink structure.
+			</p>
+			<p>
+				Second, last week we had the pleasure of co-curating our first <a href="http://wpnashville.com/blog/2014/09/18/meetup-redefining-the-limits-of-wordpress/" target="_blank">WP Nashville meetup</a>. Athletics’ Jameson Proctor and Matthew Spencer talked about our work with <a href="http://upstatement.com/timber/" target="_blank">Timber</a>, <a href="http://twig.sensiolabs.org/" target="_blank">Twig</a> and how the two come together in <a href="http://github.com/athletics/make-model" target="_blank">Make/Model</a>. Many thanks to Dale Liszka and <a href="https://twitter.com/renderandserve" target="_blank">Mark Kelnar</a> for speaking, the lovely team at <a href="http://deavor.co/" target="_blank">Deavor</a> for hosting, and to our partners at <a href="http://wpengine.com/" target="_blank">WP Engine</a> for sponsoring the event.
+			</p>'
+			)
+
+	),
+ 	array(
+ 		'slug' =>  'BANGERS & MASHUP',
+ 		'img' =>  null,
+		'title' => 'On the HiFi',
+		'content' => array(
+			'<h3><a href="https://www.google.com/url?q=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FRyan_Adams_(album)&sa=D&sntz=1&usg=AFQjCNGkgereJmveQw2kq5idnF3se3qe1Q" target="_blank">Ryan Adams <em>Ryan Adams</em></a></h3>',
+			'<p>
+				Strong Heartbreakers vibe on this record. Ryan went to the source and had <a href="https://en.wikipedia.org/wiki/Benmont_Tench" target="_blank">Benmont</a> play keys on a few tracks. Enjoyed <a href="https://www.youtube.com/watch?v=vmglUwgtHxs" target="_blank">his Letterman appearance</a>. How does Dave let you know he’s a fan? Insists on a repeat performance. Also had to laugh at the <a href="http://www.stereogum.com/1709347/watch-ryan-adams-finally-cover-bryan-adams-run-to-you/video/" target="_blank">Run to You</a> cover.
+			</p>',
+			'<h3><a href="https://en.wikipedia.org/wiki/They_Want_My_Soul" target="_blank">Spoon <em>They Want My Soul</em></a></h3>',
+			'<p>
+				Yet another good Spoon record. Speaking of Song Exploder, check out <a href="http://songexploder.net/episode-16-spoon/" target="_blank">the episode on “Inside Out”</a>.
+			</p>',
+			'<h3>Outkast — [All]</h3>',
+			'<p>
+				With a strong ATL contingent in the studio, only natural bringing Outkast back into rotation given the <a href="http://www.npr.org/blogs/therecord/2014/10/03/353516382/outkast-and-atlanta-until-they-close-the-curtain" target="_blank">recent union</a>.
+			</p>'
+		)
+ 	),
+ 	array(
+ 		'slug' =>  null,
+ 		'img' =>  null,
+		'title' => 'Random',
+		'content' => array(
+			'<h3><a href="https://vimeo.com/107469289" target="_blank">Norway Time Lapse</a></h3>'
+		)
+ 	)
+);
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+return array(
+	'stories' => $stories
+);
